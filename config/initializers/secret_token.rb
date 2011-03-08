1cb62f625473301149326b689e2969e690310330
murbapas <murbapas@bluewin.ch>
2011-03-08 13:50:35 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webshop::Application.config.secret_token = 'ef22b5aaec7f0164c336ce478fe8576d21ee7f872a73fa55c3f6dcbc1a4010d0e615f0a50764fba71b783dee6582b9a05fc94eb0c6a8ffa4117df47e633a1051'
